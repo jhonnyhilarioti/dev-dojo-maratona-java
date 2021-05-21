8e6dd85d878076a8b5aeef70153bfc6b5703bb9e
Jhonny Hilario Goulart <jhonnyhilarioti@gmail.com>
2021-05-21 13:49:31 -0300

    Removendo arquivos fora da pasta src

--- ControleDeFluxoIfElse2.java
+++
@@ -1,8 +0,0 @@
-public class ControleDeFluxoIfElse2 {
-    public static void main(String[] args) {
-        int idade = 15;
-        String status;
-        status = idade < 15 ? "Infantil" : idade>= 15 && idade< 18 ? "Juvenil" : "adulto" ;
-        System.out.println(status);
-    }
-}

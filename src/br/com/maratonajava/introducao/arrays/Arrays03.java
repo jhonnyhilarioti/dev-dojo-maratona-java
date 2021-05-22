bb0f600d5314f566ba5c08e16b9a9f242fbedfcb
Jhonny Hilario Goulart <jhonnyhilarioti@gmail.com>
2021-05-22 12:39:36 -0300

    inicializando um array na sua criação e percorrendo com foreach

--- src/br/com/maratonajava/introducao/arrays/Arrays03.java
+++ src/br/com/maratonajava/introducao/arrays/Arrays03.java
@@ -1,6 +1,6 @@
 package br.com.maratonajava.introducao.arrays;
 
-public class Arrays3 {
+public class Arrays03 {
     public static void main(String[] args) {
         String[] nomes = new String[]{"João","Maria","Goku"};
         for (String nome : nomes) {

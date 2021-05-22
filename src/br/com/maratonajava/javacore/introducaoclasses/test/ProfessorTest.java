45e771c410407e40677029a986375a1036d584c2
Jhonny Hilario Goulart <jhonnyhilarioti@gmail.com>
2021-05-22 15:43:18 -0300

    Atribuindo valores a variável professor e imprimindo esses valores

--- src/br/com/maratonajava/javacore/introducaoclasses/test/ProfessorTest.java
+++ src/br/com/maratonajava/javacore/introducaoclasses/test/ProfessorTest.java
@@ -1,2 +1,17 @@
-package br.com.maratonajava.javacore.introducaoclasses.test;public class ProfessorTest {
+package br.com.maratonajava.javacore.introducaoclasses.test;
+
+import br.com.maratonajava.javacore.introducaoclasses.classes.Professor;
+
+public class ProfessorTest {
+    public static void main(String[] args) {
+        Professor professor = new Professor();
+        professor.nome = "Leandro";
+        professor.matricula = "12152034-8";
+        professor.rg = "196.353.248-84";
+        professor.cpf = "2239574";
+        System.out.println(professor.nome);
+        System.out.println(professor.matricula);
+        System.out.println(professor.cpf);
+        System.out.println(professor.rg);
+    }
 }

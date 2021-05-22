4e2dba56495837fb9f5eee7b159f38b2463d7249
Jhonny Hilario Goulart <jhonnyhilarioti@gmail.com>
2021-05-22 15:03:13 -0300

    criando a variável do tipo carro e seus atributos

---
+++ src/br/com/maratonajava/javacore/introducaoclasses/classes/Carro.java
@@ -0,0 +1,8 @@
+package br.com.maratonajava.javacore.introducaoclasses.classes;
+
+public class Carro {
+        public String placa;
+        public String modelo;
+        public int velocidadeMaxima;
+    }
+

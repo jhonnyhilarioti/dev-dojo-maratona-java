8f43cd076b6a831cba023ee06927e9de1101b12a
Jhonny Hilario Goulart <jhonnyhilarioti@gmail.com>
2021-05-22 15:39:29 -0300

    Exercício Orientado a Objeto com variável do tipo porfessor e seu atributos

---
+++ src/br/com/maratonajava/javacore/introducaoclasses/classes/Professor.java
@@ -0,0 +1,9 @@
+package br.com.maratonajava.javacore.introducaoclasses.classes;
+
+public class Professor {
+    public String nome;
+    public String matricula;
+    public String rg;
+    public String cpf;
+
+}
---
+++ src/br/com/maratonajava/javacore/introducaoclasses/test/ProfessorTest.java
@@ -0,0 +1,2 @@
+package br.com.maratonajava.javacore.introducaoclasses.test;public class ProfessorTest {
+}

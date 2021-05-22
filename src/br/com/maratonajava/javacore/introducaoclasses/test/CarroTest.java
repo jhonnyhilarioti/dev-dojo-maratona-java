3e18ad538b5ad25a99a5f5bca58a7359d5a88e5e
Jhonny Hilario Goulart <jhonnyhilarioti@gmail.com>
2021-05-22 15:08:35 -0300

    importando a variável do tipo carro e atribuindo seus valores

--- src/br/com/maratonajava/javacore/introducaoclasses/test/CarroTest.java
+++ src/br/com/maratonajava/javacore/introducaoclasses/test/CarroTest.java
@@ -1,2 +1,15 @@
-package br.com.maratonajava.javacore.introducaoclasses.test;public class CarroTest {
+package br.com.maratonajava.javacore.introducaoclasses.test;
+
+import br.com.maratonajava.javacore.introducaoclasses.classes.Carro;
+
+public class CarroTest {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        carro.modelo = "Fusca";
+        carro.placa = "MJK61020";
+        carro.velocidadeMaxima = 300;
+        System.out.println(carro.modelo);
+        System.out.println(carro.placa);
+        System.out.println(carro.velocidadeMaxima + " km/h");
+    }
 }

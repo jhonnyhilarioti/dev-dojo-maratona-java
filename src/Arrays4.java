287c6ded46920077e02bf303c9b74a9f95c498ee
Jhonny Hilario Goulart <jhonnyhilarioti@gmail.com>
2021-05-22 10:41:32 -0300

    Percorrando um array bidimensional com fori e foreach

---
+++ src/Arrays4.java
@@ -0,0 +1,21 @@
+public class Arrays4 {
+    public static void main(String[] args) {
+        int[][] dias = new int[2][2];
+        dias[0][0] = 1;
+        dias[0][1] = 2;
+        dias[1][0] = 3;
+        dias[1][1] = 4;
+        for (int i = 0; i < dias.length ; i++) {
+            System.out.println(dias[i]);
+            for (int j = 0; j < dias[i].length ; j++) {
+                System.out.println(dias[i][j]);
+            }
+        }
+        System.out.println("----------------------------------");
+        for (int[] ref : dias){
+            for (int dia : ref){
+                System.out.println(dia);
+            }
+        }
+    }
+}

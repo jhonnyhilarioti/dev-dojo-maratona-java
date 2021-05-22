3e6c13d8544362b7c1858096d715f15eac3f64be
Jhonny Hilario Goulart <jhonnyhilarioti@gmail.com>
2021-05-22 11:28:00 -0300

    Criando um array bidimensional com tamanhos diferentes

---
+++ src/Arrays5.java
@@ -0,0 +1,14 @@
+public class Arrays5 {
+    public static void main(String[] args) {
+        int[][] dias = {{1,2},{3,4,5},{6,7,8,9,10}};
+       // dias[0] = new int[]{1,2,3};
+       // dias[1] = new int[]{4,5,6};
+      //  dias[2] = new int[]{7,8,9};
+        for (int[] arr : dias){
+            for (int num : arr){
+                System.out.println(num);
+            }
+        }
+    }
+
+}

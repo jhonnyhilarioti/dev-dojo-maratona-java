4d36c5755f72d8b62ddb29ae0948bc00519c2478
Jhonny Hilario Goulart <jhonnyhilarioti@gmail.com>
2021-05-24 13:15:02 -0300

    Retornando um método

---
+++ src/br/com/maratonajava/javacore/introducaometodos/classes/Calculadora2.java
@@ -0,0 +1,7 @@
+package br.com.maratonajava.javacore.introducaometodos.classes;
+
+public class Calculadora2 {
+    public double divideDoisNumeros(double num1, double num2){
+        return num1/num2;
+    }
+}
---
+++ src/br/com/maratonajava/javacore/introducaometodos/test/CalculadoraTest2.java
@@ -0,0 +1,13 @@
+package br.com.maratonajava.javacore.introducaometodos.test;
+
+import br.com.maratonajava.javacore.introducaometodos.classes.Calculadora2;
+
+public class CalculadoraTest2 {
+    public static void main(String[] args) {
+        Calculadora2 calc2 = new Calculadora2();
+        System.out.println(calc2.divideDoisNumeros(20,2));
+        //double result = calc2.divideDoisNumeros(20,2);
+        //System.out.println(result);
+        System.out.println("Divisão de dois números");
+    }
+}
